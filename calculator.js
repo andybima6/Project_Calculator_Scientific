8588e66f445bda75ea2bd5156ee62570b77b8e85
Andy Nugraha Putra <andybima6@gmail.com>
2024-06-21 14:12:29 +0700

    calculator scientific clear

--- calculator.js
+++ calculator.js
@@ -1,4 +1,3 @@
-
 // merubah yang mempunyai id screen ada div btn yang dimana dilakukan perulangan
 // ketika click .btn maka nilai tersebut ditambahkan di screen
 var screen = document.querySelector("#screen");
@@ -12,9 +11,67 @@ for (item of btn) {
     }
 
     if (btntext == "÷") {
-        btntext = "/";
-      }
+      btntext = "/";
+    }
 
     screen.value += btntext;
   });
+}
+
+// Logika Sin
+function sin() {
+  screen.value = Math.sin(screen.value);
+}
+
+// Logika Cos
+function cos() {
+  screen.value = Math.cos(screen.value);
+}
+// Logika Tan
+function tan() {
+  screen.value = Math.tan(screen.value);
+}
+
+// Logika Pow (Pangkat)
+function pow() {
+  screen.value = Math.pow(screen.value, 2);
+}
+
+// Logika SQRT (Akar Kuadrt)
+function sqrt() {
+  screen.value = Math.sqrt(screen.value, 2);
+}
+
+// Logika Log
+function log() {
+  screen.value = Math.log(screen.value, 2);
+}
+
+// Logika pi
+function pi() {
+  (screen.value = 3), 141592;
+}
+
+// Logika e (bilangan Euler)
+function e() {
+  (screen.value = 2), 71828;
+}
+
+// Logika faktorial
+function fact() {
+  var i, num, f;
+
+  f = 1;
+  num = screen.value;
+  for (i = 1; i <= num; i++) {
+    f = f * i;
+  }
+  i = i - 1;
+
+  screen.value = f;
+}
+
+// Logika Backspace
+function backspace() {
+    screen.value = screen.value.substr(0,screen.value.length)
 }
